4f5279d0819c62c266bbc972fc38928afa575a84
Gerardo Manzo <programmingmanzo@gmail.com>
2019-03-27 08:12:04 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,3 @@
-class Dog 
-  def initialize(breed)
-    @breed = breed 
-  end
-  
-  def breed=(breed)
-    @breed = breed 
-  end
-  
-  def breed 
-    @breed 
-  end 
-end
-
 class Person
   def initialize(name)
     @name = name 
